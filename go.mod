8975bb7d581e30bc53e4d963d39c1d4553d84c20
Filip Wojciechowski <filip@acuitymd.com>
2025-03-18 15:46:01 -0700

    initial

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/fwojciec/goadbcpgrepro
+
+go 1.24.1
+
+require (
+	github.com/apache/arrow-adbc/go/adbc v1.5.0
+	github.com/apache/arrow-go/v18 v18.2.0
+)
+
+require (
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/flatbuffers v25.2.10+incompatible // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/tools v0.31.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/apache/arrow-adbc/go/adbc"
+	"github.com/apache/arrow-adbc/go/adbc/drivermgr"
+	"github.com/apache/arrow-go/v18/arrow/flight"
+	"github.com/apache/arrow-go/v18/arrow/ipc"
+)
+
+func main() {
+	if err := runServer(); err != nil {
+		panic(err)
+	}
+}
+
+type server struct {
+	flight.BaseFlightServer
+}
+
+func (s *server) DoGet(tkt *flight.Ticket, fs flight.FlightService_DoGetServer) error {
+	pgDsn := os.Getenv("POSTGRESQL_DSN")
+	if pgDsn == "" {
+		return fmt.Errorf("POSTGRESQL_DSN is not set")
+	}
+
+	var drv drivermgr.Driver
+	db, err := drv.NewDatabase(map[string]string{
+		"driver":          "adbc_driver_postgresql",
+		adbc.OptionKeyURI: pgDsn,
+	})
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	conn, err := db.Open(fs.Context())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	st, err := conn.NewStatement()
+	if err != nil {
+		return err
+	}
+	defer st.Close()
+
+	if err := st.SetSqlQuery("SELECT * FROM synthetic_filter_option"); err != nil {
+		return err
+	}
+
+	reader, _, err := st.ExecuteQuery(fs.Context())
+	if err != nil {
+		return err
+	}
+	reader.Retain()
+	defer reader.Release()
+
+	wr := flight.NewRecordWriter(
+		fs,
+		ipc.WithSchema(reader.Schema()),
+	)
+	defer wr.Close()
+
+	for reader.Next() {
+		rec := reader.Record()
+		if err := wr.Write(rec); err != nil {
+			return fmt.Errorf("couldn't write record: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func runServer() error {
+	s := flight.NewServerWithMiddleware(nil)
+	s.Init("0.0.0.0:8816")
+	s.RegisterFlightService(&server{})
+
+	return s.Serve()
+}
